2ca77bda30a33ee59d455613f03dc98de6612e50
ArianJafari <arian.jafari2233@gmail.com>
2025-10-24 16:32:50 +0330

    comment api connected, channel api connected

--- src/components/play-video/video-player.jsx
+++ src/components/play-video/video-player.jsx
@@ -6,21 +6,56 @@ import save from '../../assets/save.png';
 import jack from '../../assets/jack.png';
 import user_profile from '../../assets/user_profile.jpg';
 
+import {API_KEY, value_convertor} from '../../data.js';
+
 import './video-player.css';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment';
 
 const VideoPlayer = ({videoId}) => {
-    
+    const [apiData, setApiData] = useState(null);
+    const [channelData, setChannelData] = useState(null);
+    const [commentData, setCommentData] = useState([]);
+
+    const fetchVideoData = async () => {
+        const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+        
+        await fetch(videoDetails_url)
+        .then(response => response.json())
+        .then(data => setApiData(data.items[0]));
+
+    }
+
+    const fetchOtherData = async () => {
+        const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+        await fetch (channelData_url)
+        .then(response => response.json())
+        .then(data => setChannelData(data.items[0]));
+
+        const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${videoId}&key=${API_KEY}`;
+        await fetch(comment_url)
+        .then(res => res.json())
+        .then(data => setCommentData(data.items))
+    }
+
+    useEffect (() => {
+        fetchVideoData()
+    }, []);
+
+    useEffect(() => {
+        fetchOtherData()
+    }, [apiData])
+
   return (
     <div className='play-video'>
         {/* <video src={video1} controls autoPlay muted></video> */}
         <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <h3>Best Yooutube Channel to Laern Web Development</h3>
+        <h3>{apiData ? apiData.snippet.title : ""}</h3>
         <div className='play-video-info'>
-            <p>1525 Views &bull; 2days ago</p>
+            <p>{apiData? value_convertor(apiData.statistics.viewCount) : "16k"} Views &bull;{apiData? moment(apiData.snippet.publishedAt).fromNow() : ''} </p>
             <div>
-                <span><img src={like} alt="" />125</span>
+                <span><img src={like} alt="" />{apiData ? value_convertor(apiData.statistics.likeCount) : ''}</span>
                 <span><img src={dislike} alt="" />2</span>
                 <span><img src={share} alt="" />Share</span>
                 <span><img src={save} alt="" />Save</span>
@@ -28,78 +63,35 @@ const VideoPlayer = ({videoId}) => {
         </div>
             <hr />
             <div className='publisher'>
-                <img src={jack} alt="" />
+                <img src={channelData ? channelData.snippet.thumbnails.default.url : ''} alt="" />
                 <div>
-                    <p>GreatStack</p>
-                    <span>1 million subscribers</span>
+                    <p>{apiData ? apiData.snippet.channelTitle : ''}</p>
+                    <span>{channelData ? value_convertor(channelData.statistics.subscriberCount) : ''} subscribers</span>
                 </div>
                 <button>Subscribe</button>
             </div>
             <div className="vid-descripsion">
-                <p>Channel that makes learning easy</p>
-                <p>Subscribe GreatStack to Watch More Tutorials on web Development</p>
+                <p>{apiData ? apiData.snippet.description.slice(0, 250) : 'Description here'}</p>
                 <hr />
-                <h4>130 Comments</h4>
-                <div className='comment'>
-                    <img src={user_profile} alt="" />
-                    <div>
-                        <h3>Jack Nicholson <span>1 day ago</span></h3>
-                        <p>A global computer network providing a variety of Information and cc of interconnected networks using standardize commuication protocols. </p>
-                        <div className='comment-action'>
-                            <img src={like} alt="" />
-                            <span>244</span>
-                            <img src={dislike} alt="" />
+                <h4>{apiData ? value_convertor(apiData.statistics.commentCount) : ''} Comments</h4>
+                {commentData.map((item, index) => {
+                    return (
+                        <div className='comment' key={index}>
+                            <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+                            <div>
+                                <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>1 day ago</span></h3>
+                                <p>A global computer network providing a variety of Information and cc of interconnected networks using standardize commuication protocols. </p>
+                                <div className='comment-action'>
+                                    <img src={like} alt="" />
+                                    <span>{ value_convertor(item.snippet.topLevelComment.snippet.likeCount) }</span>
+                                    <img src={dislike} alt="" />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className='comment'>
-                    <img src={user_profile} alt="" />
-                    <div>
-                        <h3>Jack Nicholson <span>1 day ago</span></h3>
-                        <p>A global computer network providing a variety of Information and cc of interconnected networks using standardize commuication protocols. </p>
-                        <div className='comment-action'>
-                            <img src={like} alt="" />
-                            <span>244</span>
-                            <img src={dislike} alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div className='comment'>
-                    <img src={user_profile} alt="" />
-                    <div>
-                        <h3>Jack Nicholson <span>1 day ago</span></h3>
-                        <p>A global computer network providing a variety of Information and cc of interconnected networks using standardize commuication protocols. </p>
-                        <div className='comment-action'>
-                            <img src={like} alt="" />
-                            <span>244</span>
-                            <img src={dislike} alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div className='comment'>
-                    <img src={user_profile} alt="" />
-                    <div>
-                        <h3>Jack Nicholson <span>1 day ago</span></h3>
-                        <p>A global computer network providing a variety of Information and cc of interconnected networks using standardize commuication protocols. </p>
-                        <div className='comment-action'>
-                            <img src={like} alt="" />
-                            <span>244</span>
-                            <img src={dislike} alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div className='comment'>
-                    <img src={user_profile} alt="" />
-                    <div>
-                        <h3>Jack Nicholson <span>1 day ago</span></h3>
-                        <p>A global computer network providing a variety of Information and cc of interconnected networks using standardize commuication protocols. </p>
-                        <div className='comment-action'>
-                            <img src={like} alt="" />
-                            <span>244</span>
-                            <img src={dislike} alt="" />
-                        </div>
-                    </div>
-                </div>
+                    )
+                })}
+                
+                
             
         </div>
     </div>
